8fbde443e60a20aec8d131180b96a72f0f262400
alexis jardin <alexis.david.jardin@gmail.com>
2024-04-04 14:57:47 -0300

    add: logo de la aplicacion

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="flex justify-center pt-4">
       <Image
-        src="medilatam.svg"
+        src="/Medilatam.svg"
         alt="logo Medilatam"
         width={1000}
         height={760} />
