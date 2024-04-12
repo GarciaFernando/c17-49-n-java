6427aac2013a508565b65e05a56e8e1f442e6f0f
willCh <willchcode@gmail.com>
2024-04-12 00:47:28 -0400

    HOMEPAGE especialidad, profesional

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,15 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Background } from "./ui/layout/Background";
-import { Navbar } from "./ui/layout/Navbar";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Background } from './ui/layout/Background';
+import { Navbar } from './ui/layout/Navbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Medilatam",
-  description: "Tu portal de salud en América Latina. Conectamos a pacientes con profesionales médicos, ofreciendo consultas virtuales, herramientas de seguimiento y acceso a información médica confiable. Tu guía para una salud óptima en la región",
+  title: 'Medilatam',
+  description:
+    'Tu portal de salud en América Latina. Conectamos a pacientes con profesionales médicos, ofreciendo consultas virtuales, herramientas de seguimiento y acceso a información médica confiable. Tu guía para una salud óptima en la región',
 };
 
 export default function RootLayout({
@@ -20,14 +21,16 @@ export default function RootLayout({
     <html lang="es">
       <link rel="icon" href="/favicon.svg" />
       <body className={`bg-slate-200 ${inter.className}`}>
-        <header>
+        {/* <header className="borde1">
           <Background />
-        </header>
-        <section className="flex flex-col w-[300px] h-[550px] p-2 mb-0 mx-6 gap-6 -mt-[180px] items-center bg-white rounded-xl relative shadow-lg">
-          <Navbar />
-        </section>
-        {children}
+        </header> */}
+        <div className="grid grid-cols-[350px_minmax(0,1fr)] h-[100vh]">
+          <header className=" ">
+            <Navbar />
+          </header>
+          <main className="borde2 w-full">{children}</main>
+        </div>
       </body>
-    </html >
+    </html>
   );
 }
