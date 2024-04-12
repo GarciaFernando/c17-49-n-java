538d2803af104031dccc3002bfd87acfb481bd07
alexis jardin <alexis.david.jardin@gmail.com>
2024-04-11 22:22:50 -0300

    add: icon home

--- frontend/app/ui/layout/Navbar.tsx
+++ frontend/app/ui/layout/Navbar.tsx
@@ -2,6 +2,13 @@ import Image from "next/image"
 import { ImagenAvatar } from "./ImagenAvatar"
 import Link from "next/link"
 
+const links=[
+    { name: 'Reserva de turnos', href: '#', icon: "calendar.svg" },
+    { name: 'Mis consultas', href: '#', icon: "nota.svg" },
+    { name: 'Perfil', href: '#', icon: "user.svg" },
+    { name: 'Atención virtual', href: '#', icon: "celular.svg" },
+]
+
 export const Navbar = () => {
     return (
         <>
@@ -20,13 +27,21 @@ export const Navbar = () => {
                     <button type="button" className="text-white bg-mlt-600 hover:bg-mlt-800 focus:outline-none font-medium rounded-full text-sm px-6 py-1 text-center me-2 mb-2">Registro</button>
                 </div>
             </div>
-            <nav className="flex flex-col gap-2 items-center text-2xl">
-                <Link className="flex h-[48px] grow items-center gap-2 rounded-md bg-gray-5 text-sm font-medium hover:bg-mlt-600 hover:text-mlt-100 flex-none justify-star px-16" href="#">Reserva de turnos</Link>
-                <Link className="flex h-[48px] grow items-center gap-2 rounded-md bg-gray-5 text-sm font-medium hover:bg-mlt-600 hover:text-mlt-100 flex-none justify-star px-20" href="#">Mis consultas</Link>
-                <Link className="flex h-[48px] grow items-center gap-2 rounded-md bg-gray-5 text-sm font-medium hover:bg-mlt-600 hover:text-mlt-100 flex-none justify-star px-[110px]" href="#">Perfil</Link>
-                <Link className="flex h-[48px] grow items-center gap-2 rounded-md text-sm font-medium hover:bg-mlt-600 hover:text-mlt-100 flex-none justify-star px-[75px]" href="#">Atención virtual</Link>
-            </nav >
-
-        </>
+            <nav className="flex flex-col grow space-x-0 space-y-2 w-full">
+            {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className="flex h-[48px] grow items-center justify-start gap-2 rounded-md p-3 text-sm font-medium hover:bg-mlt-600 hover:text-mlt-100"
+          >
+            <Image src={`/icon/${LinkIcon}`} alt={LinkIcon} className="w-6" width={32} height={32}/>
+            <p className="hidden md:block">{link.name}</p>
+          </Link >
+        );
+      })} 
+    </nav >
+</>
     )
 }
