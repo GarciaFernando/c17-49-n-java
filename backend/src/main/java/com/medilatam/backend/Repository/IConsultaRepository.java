165436fe49afd379d5c149319ee54beb0b11ad0f
Anthony <antony.medinagarcia24@gmail.com>
2024-04-13 00:55:34 -0600

    add: Entidad ConsultaRequest para mapearla a Consulta add: Métodos createConsulta, deleteConsulta, editConsulta

--- backend/src/main/java/com/medilatam/backend/Repository/IConsultaRepository.java
+++ backend/src/main/java/com/medilatam/backend/Repository/IConsultaRepository.java
@@ -4,7 +4,12 @@ import com.medilatam.backend.Entity.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface IConsultaRepository extends JpaRepository<Consulta,Long> {
+
+    boolean findByFecha(Date fecha);
 
 }
