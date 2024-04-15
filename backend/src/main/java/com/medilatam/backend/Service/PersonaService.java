2bcc45f7200f007164493d771683d475c5a28608
ChristianP-7 <christianperezt7@gmail.com>
2024-04-15 00:01:52 -0500

    Feat(Backend): Get, GetById y Post para PersonaEntity

--- backend/src/main/java/com/medilatam/backend/Service/PersonaService.java
+++ backend/src/main/java/com/medilatam/backend/Service/PersonaService.java
@@ -1,12 +1,35 @@
 package com.medilatam.backend.Service;
 
+import com.medilatam.backend.Entity.PersonaEntity;
+import com.medilatam.backend.Interface.IPersonaService;
 import com.medilatam.backend.Repository.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 
-public class PersonaService {
+public class PersonaService implements IPersonaService {
     @Autowired
-    private PersonaRepository repository;
+    private PersonaRepository personaRepository;
+
+    @Override
+    public List<PersonaEntity> listarPersonas() {
+        List<PersonaEntity> personaEntities = personaRepository.findAll();
+        return personaEntities;
+    }
+
+    @Override
+    public PersonaEntity personaPorId(Long id) {
+        PersonaEntity personaEntity = personaRepository.findById(id).orElse(null);
+        return personaEntity;
+    }
+
+    @Override
+    public void guardarPersona(PersonaEntity personaEntity) {
+        personaRepository.save(personaEntity);
+    }
+
+
 }
